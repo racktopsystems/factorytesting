26c4e7af685e7219df276ba102af35d81378c8f4
Sam Zaydel <szaydel@gmail.com>
2018-12-11 15:35:28 -0800

    Focus on drives and hwadm output

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # factorytesting
 
 Basic santy-checking tools for Brickstor hardware.
+
+Intent here is to be able to run these on appliance and not need anything further.
+
+To run __healthcheck.py__ script, we can just `curl` it and pass to python interpreter on stdin:
+```
+# curl -ks https://raw.githubusercontent.com/racktopsystems/factorytesting/master/healthcheck.py | python
+```
--- healthcheck.py
+++ healthcheck.py
@@ -22,6 +22,7 @@
 #
 # Copyright 2018 RackTop Systems.
 
+import datetime
 import os
 import subprocess
 import unittest
@@ -29,11 +30,47 @@ import json
 
 os_guid = u"6b7e3683761ee397e78eb688222d8d5a"
 
+# def is_known_drive_vendor(mfg):
+#     m = {
+#         "hgst": 1,
+#         "hitachi": 1,
+#         "seagate": 1,
+#     }
+#     return mfg.lower() in m
+
 class BasicSystemSanity(unittest.TestCase):
+    _hwinfo = []
+    
+    def known_drive_vendor(self, mfg):
+        m = {
+            "hgst": 1,
+            "hitachi": 1,
+            "seagate": 1,
+        }
+        return mfg.lower() in m
+
+    def skip_drive_ok(self, make):
+        should_skip = ("ata") # Add others here in the future
+        for entry in should_skip:
+            if entry in make.lower():
+                return True
+        return False
+
+    def drive_type_sensible(self, t):
+        return t.lower() in ("ssd", "hdd")
+
+    @classmethod
+    def hwinfo(cls):
+        return cls._hwinfo
+
     @classmethod
     def setUpClass(cls):
-        pass # We don't need this for the time being
-    
+        # We will refer to this information multiple times.
+        output = subprocess.check_output(
+            ["/usr/racktop/sbin/hwadm", "-j", "ls", "d"]
+        )
+        cls.hwinfo = json.loads(output)
+
     @classmethod
     def tearDownClass(cls):
         pass # We don't need this for the time being
@@ -64,21 +101,37 @@ class BasicSystemSanity(unittest.TestCase):
             "Expected no output, instead one or more services is not healthy")
 
     def test_hwd_is_online(self):
+        """ hwd service must always be online """
         output = subprocess.check_output(
             ["/usr/bin/svcs", "-H", "-o", "state", "hwd"])
         self.assertEqual(output.rstrip('\n'), "online",
-            "Output must match string 'online'")
-
-    def test_dataprotectiond_is_online(self):
-        pass
-
-    def test_datareplicationd_is_online(self):
-        pass
+            "Expected 'online' got '%s'" % output.rstrip('\n'))
 
     def test_secured_is_online(self):
-        """ Check if secured service is online """
+        """ secured service must always be online """
         output = subprocess.check_output(
             ["/usr/bin/svcs", "-H", "-o", "state", "secured"])
+        self.assertEqual(output.rstrip('\n'), "online",
+            "Expected 'online' got '%s'" % output.rstrip('\n'))
+
+    def test_dataprotectiond_is_online(self):
+        """ dataprotectiond service must always be online """
+        output = subprocess.check_output(
+            ["/usr/bin/svcs", "-H", "-o", "state", "dataprotectiond"])
+        self.assertEqual(output.rstrip('\n'), "online",
+            "Expected 'online' got '%s'" % output.rstrip('\n'))
+
+    def test_datareplicationd_is_disabled(self):
+        """ datareplicationd service must always be disabled """
+        output = subprocess.check_output(
+            ["/usr/bin/svcs", "-H", "-o", "state", "datareplicationd"])
+        self.assertEqual(output.rstrip('\n'), "disabled",
+            "Expected 'disabled' got '%s'" % output.rstrip('\n'))
+
+    def test_bsrapid_is_online(self):
+        """ bsrapid service must always be online """
+        output = subprocess.check_output(
+            ["/usr/bin/svcs", "-H", "-o", "state", "bsrapid"])
         self.assertEqual(output.rstrip('\n'), "online",
             "Expected 'online' got '%s'" % output.rstrip('\n'))
 
@@ -97,6 +150,15 @@ class BasicSystemSanity(unittest.TestCase):
             ["/usr/racktop/sbin/bsradm", "dns", "domain", "get"]
         )
         self.assertIsNot(output, "")
+
+    def test_only_one_image_installed(self):
+        """ Only a single OS image should be loaded """
+        output = subprocess.check_output(
+            ["/usr/racktop/sbin/bsradm", "-j", "os", "installed"])
+        j = json.loads(output.rstrip('\n'))
+        self.assertEqual(len(j), 1,
+            "Expected to find only a single OS image, " \
+            "instead found '%d' images" % len(j))
 
     def test_os_version_expected(self):
         output = subprocess.check_output(
@@ -191,7 +253,65 @@ class BasicSystemSanity(unittest.TestCase):
 
     def test_no_scsi_vhci_errors_expected(self):
         pass
-    
+
+    def test_hwadm_drive_attributes_expected(self):
+        """ Check drive count and basic attributes are sensible """
+        now = datetime.datetime.now()
+        # Check that attributes of device make sense
+        self.assertGreaterEqual(len(self.hwinfo), 12)
+        for i in self.hwinfo:
+            if self.skip_drive_ok(i[u'Make']):
+                continue # Skip devices that we don't expect to be used for pool
+            self.assertTrue(self.known_drive_vendor(i[u'Make']),
+                "Encountered unexpected drive make: '%s'" % i[u'Make'])
+            ts = i[u'HWInfo'][u'RegistrationTimestamp'][:-5]
+            regts = datetime.datetime.strptime(ts, "%Y-%m-%dT%H:%M:%S")
+            reg_stat = i[u'HWInfo'][u'RegistrationStatus']
+            ready_stat = i[u'HWInfo'][u'ReadyStatus']
+
+            # We will see how this works with SSDs
+            self.assertFalse(reg_stat == u"NotSupported",
+                "Registration should be supported on this drive")
+            if reg_stat == u"Registered":
+                # Nothing should be < 2017
+                self.assertGreaterEqual(regts.year, 2017)
+                # One years of sway
+                self.assertLessEqual(now.year - regts.year, 1)
+                # Registration timestamp cannot be later than time now
+                self.assertTrue(now > regts,
+                    "Registration timestamp cannot be more recent "\
+                    "than present time")
+
+            # Device name should be a part of a the device path.
+            self.assertEqual(i[u'Path'], "/dev/rdsk/%ss0" % i[u'DeviceName'])
+            # Model, Serial cannot be empty
+            self.assertIsNot(i[u'Serial'], "")
+            self.assertGreaterEqual(i[u'Serial'], 8)
+            self.assertIsNot(i[u'Model'], "")
+            # Some things have an expected length
+            self.assertEqual(len(i[u'StorageUnitId']), 16)
+            self.assertEqual(len(i[u'Wwn']), 16)
+            # Bay number should be a nonnegative value and 83 is largest
+            # possible currently, since largest shelf has 84 bays.
+            bay_min_idx, bay_max_idx = 0, 83
+            bay_idx = i[u'HWInfo'][u'Bay']
+            temp_c = i[u'HWInfo'][u'CelsiusTemperature']
+            temp_max_c = i[u'HWInfo'][u'MaxFunctionalTemp']            
+            self.assertGreaterEqual(i[u'HWInfo'][u'Bay'], bay_min_idx,
+                "Bay number cannot be lower than '%d', " % bay_min_idx)
+            self.assertLessEqual(i[u'HWInfo'][u'Bay'], bay_max_idx,
+                "Bay number cannot be greater than '%d'" % bay_max_idx)
+            # Temperature sensors should be reporting something sensible
+            self.assertGreaterEqual(temp_c, 0,
+                "Temperature cannot be negative")
+            self.assertGreaterEqual(temp_max_c, 0,
+                "Maximum operating temperature cannot be negative")
+            # Current temp cannot be greater than maximum operating temp
+            self.assertLessEqual(temp_c, temp_max_c)
+            self.assertTrue(self.drive_type_sensible(i[u'HWInfo'][u'Type']),
+                "Only SSDs and HDDs are allowed drive types")
+
+
 class CustomTextTestResult(unittest.TextTestResult):
     def addSuccess(self, test):
         if self.showAll:
